74365c40d6ce45ddbeaf2412dc8ee3b95ed0484b
AdriaVidal-24 <adriavidal@paucasesnovescifp.cat>
2026-04-29 17:13:45 +0200

    feat(js): add task creation form functionality

---
+++ js/form-tasca.js
@@ -0,0 +1,34 @@
+import saveTask from "./storage.js";
+import Task from "./models.js";
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    const taskForm = document.getElementById("task");
+    const submitButton = document.getElementById("task-submit");
+
+    submitButton.addEventListener("click", function(event) {
+        event.preventDefault;
+
+        const taskName = document.getElementById("task-name");
+        const taskDescription = document.getElementById("task-description");
+        const taskDate = document.getElementById("task-date");
+        const taskCategory = document.getElementById("task-category");
+        const taskPriority = document.getElementById("task-priority");
+
+        const taskItem = new Task();
+
+        taskItem.name = taskName.value;
+        taskItem.desc = taskDescription.value;
+        taskItem.date = taskDate.value;
+        taskItem.category = taskCategory.value;
+        taskItem.priority = taskPriority.value;
+
+        taskName.value = '';
+        taskDescription.value = '';
+        taskDate.value = '';
+
+        console.log(taskItem);
+        taskItem.saveTask;
+    })
+
+})
