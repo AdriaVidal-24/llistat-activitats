dd6cb6736e09bcc1c70850526b8f298068bddb9e
AdriaVidal-24 <adriavidal@paucasesnovescifp.cat>
2026-05-05 21:14:48 +0200

    feat(js): print tasks to index.hmtl

--- js/index.js
+++ js/index.js
@@ -3,5 +3,18 @@ import {getTasks} from "./storage.js";
 
 document.addEventListener("DOMContentLoaded", function(){
 
+    function printTasks() {
+        const tasks = getTasks();
+
+        const container = document.getElementById('task-list');
+        container.innerHTML = '';
+
+        tasks.forEach(task => {
+            task = new Task(task.name, task.desc, task.date, task.category, task.priority);
+            task.printTask();
+        })
+    }
+
+    printTasks();
 
 })
