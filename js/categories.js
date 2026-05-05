28a52d81e35229eea8a838434358aaef3adad934
AdriaVidal-24 <adriavidal@paucasesnovescifp.cat>
2026-05-05 21:14:17 +0200

    feat(js): add category creation js

--- js/categories.js
+++ js/categories.js
@@ -3,5 +3,41 @@ import {Category} from "./models.js";
 
 document.addEventListener("DOMContentLoaded", function(){
 
+    function printCategories() {
+        const categories = getCategories();
+
+        const container = document.getElementById('cat-list');
+        container.innerHTML = '';
+
+        categories.forEach(cat => {
+            cat = new Category(cat.name, cat.color);
+            cat.printCategory();
+        })
+    }
+
+    printCategories();
+
+
+    const categoryForm = document.getElementById("category");
+    const submitButton = document.getElementById("cat-submit");
+
+    submitButton.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        const categoryName = document.getElementById("cat-name");
+        const categoryColor = document.getElementById("cat-color");
+
+        const categoryItem = new Category();
+
+        categoryItem.name = categoryName.value;
+        categoryItem.color = categoryColor.value;
+
+        categoryName.value = '';
+        categoryColor.value = '#000000';
+
+        console.log(categoryItem);
+        saveCategory(categoryItem.toObject());
+        printCategories();
+    })
 
 })
