6a7984118cf4429cb28dd31ad4e2870a244694ca
AdriaVidal-24 <adriavidal@paucasesnovescifp.cat>
2026-05-06 16:53:43 +0200

    feat(js): add deleting task & category function

--- js/storage.js
+++ js/storage.js
@@ -5,8 +5,10 @@ export function getTasks() {
 }
 
 export function saveTask(task) {
+    console.log(task);
     let taskListAux = getTasks();
     taskListAux.push(task);
+    console.log(taskListAux);
     taskListAux = JSON.stringify(taskListAux);
     localStorage.setItem('tasks', taskListAux);
 }
@@ -14,6 +16,19 @@ export function saveTask(task) {
 export function deleteTask(id) {
     let taskListAux = getTasks();
     taskListAux = taskListAux.filter(task => id != task.id);
+    taskListAux = JSON.stringify(taskListAux);
+    localStorage.setItem('tasks', taskListAux);
+}
+
+export function toggleTask(id) {
+    let taskListAux = getTasks();
+    taskListAux = taskListAux.map(task => {
+        if(task.id === id) {
+            task.done = !task.done;
+        }
+        return task;
+    });
+    taskListAux = JSON.stringify(taskListAux);
     localStorage.setItem('tasks', taskListAux);
 }
 
@@ -30,8 +45,9 @@ export function saveCategory(category) {
     localStorage.setItem('categories', categoryListAux);
 }
 
-export function deleteCategory(id) {
+export function deleteCategory(name) {
     let categoryListAux = getCategories();
-    categoryListAux = categoryListAux.filter(category => id != category.id);
+    categoryListAux = categoryListAux.filter(category => name != category.name);
+    categoryListAux = JSON.stringify(categoryListAux);
     localStorage.setItem('categories', categoryListAux);
 }
