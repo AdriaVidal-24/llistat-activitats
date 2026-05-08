5df125315c2a613cdf456bb30c76701653973cf5
AdriaVidal-24 <adriavidal@paucasesnovescifp.cat>
2026-05-08 18:27:32 +0200

    feat: add adding and removing chart tasks

--- js/grafics.js
+++ js/grafics.js
@@ -3,7 +3,21 @@ import { getTasksMonth, saveTasksMonth } from "./storage.js";
 const month = new Date().getMonth();
 const monthLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 let chart = null;
+
+export function addTaskMonth() {
     const data = getTasksMonth();
+    data[month]++;
+    saveTasksMonth(data);
+}
+
+export function removeTaskMonth() {
+    const data = getTasksMonth();
+    if(data[month] > 0) {
+        data[month]--;
+    }
+    saveTasksMonth(data);
+}
+
 export function printChart() {
     const ctx = document.getElementById('Graph');
     const data = getTasksMonth();
